335e68f04360cbe6451680b4bd6d993414ea9309
skshani <skshani4930@gmail.com>
2024-10-16 17:54:01 +0530

    addImage

--- src/ButtonT.jsx
+++ src/ButtonT.jsx
@@ -1,25 +1,41 @@
-
+import imgA from './img/java.jpeg';
+import reactImg from './img/react2.jpeg';
+import React from 'react'
 
 function ButtonT() {
-    let counter = 1;
-
-    const add = () => {
-   const setCounter = counter+1;
-   console.log(setCounter);
-   
-
-    }
     return (
-        <>
-            <h2>Counter Value :{counter}</h2>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ea maiores illum dolorem provident a quos deserunt blanditiis temporibus obcaecati nulla tempore cum accusamus nihil nisi voluptate, illo cupiditate, autem odit?</p>
-            <button onClick={add()}>Add Value</button>
-            <button onClick={add()}>sum</button>
-            <button onClick={add()}>sum</button>
-            <button onClick={add()}>sum</button>
-            <button onClick={add()}>sum</button>
+        <div>
 
-        </>
+            <h1>Java Programming Language</h1>
+
+            <img src={imgA} alt="shani" />
+
+            <h1>React JS BY Facebook</h1>
+            <img src={reactImg} alt="hello" />
+
+            <h1>React Open Source Library</h1>
+            <img src="photo/react1.jpeg" alt="shani" />
+
+            <h1>React Book</h1>
+            <img src="photo/react2.jpeg" alt="" />
+
+            <h1>React Book</h1>
+            <img src="photo/react2.jpeg" alt="" />
+
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti alias,
+                saepe placeat blanditiis ab culpa esse fugiat similique cum nesciunt, praesentium in.
+                Voluptatum cupiditate
+                placeat consectetur. Beatae ratione consequuntur natus!</p>
+
+            <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis aliquam aspernatur
+                officiis eum, corrupti quae et dicta quia, praesentium minima consectetur commodi debitis!
+                Quidem quaerat iusto mollitia cupiditate adipisci. Consectetur corporis quo dolorum soluta dolore
+                minima, error aliquam. Voluptas repellendus sed commodi quos tenetur recusandae molestiae debitis
+                beatae nobis aliquid placeat nostrum dolore vel asperiores, vero atque accusamus! Optio incidunt at doloremque dignissimos? Aliquid ullam dolorem labore, est aut quidem tempora totam nulla nam quasi dolorum explicabo corrupti tempore laboriosam expedita veniam illum suscipit impedit molestiae similique, deleniti, atque mollitia et. Eum hic veritatis aliquam sint! Enim rem ullam reprehenderit inventore quisquam fugit laboriosam dolore. Minus molestiae sequi dicta provident commodi quisquam iure, odio adipisci, voluptatem et, tenetur qui. Dolore, ad reprehenderit? Iste voluptas fuga nulla sit eos veritatis quis alias doloremque praesentium similique, eum recusandae voluptatum at! Pariatur molestiae dolore vero autem ad accusantium, ipsam hic alias cumque eligendi quis voluptate ducimus neque atque delectus facere porro, vel obcaecati blanditiis, numquam aliquam? Consectetur minima voluptatibus voluptas corrupti vel aut labore, sed nam vitae. Officia, impedit ad. Ut dolores obcaecati iste voluptatem, et fuga laborum sint mollitia atque accusamus? Vitae enim consectetur beatae aut!
+                Dolorum eos quas voluptatem iure laboriosam.</h4>
+
+        </div>
     )
 }
+
 export default ButtonT
